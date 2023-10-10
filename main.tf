16f4c8dae055d4778b2333777647f777bb734041
Erwan Arzur <erwan@bladelabs.io>
2023-10-10 14:02:49 +0200

    add support for is_secret_data_base64

--- main.tf
+++ main.tf
@@ -84,4 +84,5 @@ resource "google_secret_manager_secret_version" "secret-version" {
   for_each    = { for secret in var.secrets : secret.name => secret }
   secret      = google_secret_manager_secret.secrets[each.value.name].id
   secret_data = each.value.secret_data
+  is_secret_data_base64 = lookup(each.value, "is_secret_data_base64", false)
 }
