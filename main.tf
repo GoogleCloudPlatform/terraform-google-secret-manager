5f78bea92bbd13734e3488c18e6edc973ff46bd3
ps-occrp <138824595+ps-occrp@users.noreply.github.com>
2024-03-05 17:28:40 +0100

    feat: Make secrets data optional (#61)

--- main.tf
+++ main.tf
@@ -84,7 +84,7 @@ resource "google_secret_manager_secret" "secrets" {
 }
 
 resource "google_secret_manager_secret_version" "secret-version" {
-  for_each    = { for secret in var.secrets : secret.name => secret }
+  for_each    = { for secret in var.secrets : secret.name => secret if can(secret.secret_data) }
   secret      = google_secret_manager_secret.secrets[each.value.name].id
   secret_data = each.value.secret_data
 }
