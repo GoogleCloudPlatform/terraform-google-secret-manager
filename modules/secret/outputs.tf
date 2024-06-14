54775c014476540afcae39da3a71050df4d754bd
Mikhail Zholobov <legal90@gmail.com>
2024-06-14 12:34:57 +0200

    Add "project_id" output to the "secret" module

--- modules/secret/outputs.tf
+++ modules/secret/outputs.tf
@@ -28,3 +28,8 @@ output "version" {
   description = "The version of the created secret"
   value       = google_secret_manager_secret_version.version.name
 }
+
+output "project_id" {
+  description = "GCP Project ID where secret was created"
+  value       = google_secret_manager_secret.secret.project
+}
