7952b2631fd6ffc4575cb07155b00b16a681c5e3
Bharath KKB <bharathkrishnakb@gmail.com>
2025-01-14 10:53:59 -0600

    chore: fix attrib (#122)

--- versions.tf
+++ versions.tf
@@ -28,6 +28,6 @@ terraform {
   }
 
   provider_meta "google" {
-    module_name = "blueprints/terraform/secret-manager/v0.7.0"
+    module_name = "blueprints/terraform/terraform-google-secret-manager/v0.7.0"
   }
 }
